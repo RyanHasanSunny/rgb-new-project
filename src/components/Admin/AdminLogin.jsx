f62f08e4215272bce9a47f9b849bcf6602c3d25e
Ryan Hasan Sunny <ryanhasansunny31@gmail.com>
2025-08-26 04:38:23 +0600

    Update AdminLogin.jsx

--- src/components/Admin/AdminLogin.jsx
+++ src/components/Admin/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../../Styles/AdminLogin.css";
+import "../../styles/AdminLogin.css";
 import { auth } from "../../firebaseConfig";
 import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
